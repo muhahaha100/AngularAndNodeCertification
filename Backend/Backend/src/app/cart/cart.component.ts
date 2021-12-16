95d06c3a1030263ecdfb25ba543a944ed94ecacf
nlama002 <nlama002@citymail.cuny.edu>
2021-12-16 14:18:29 -0500

    checkout

---
+++ Backend/Backend/src/app/cart/cart.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.css']
+})
+export class CartComponent implements OnInit {
+
+  cartItems = [
+    
+    { id: 1, productId: 1, productName: 'a', qty: 2, price: 100},
+    { id: 2, productId: 2, productName: 'b', qty: 3, price: 50},
+    { id: 3, productId: 3, productName: 'c', qty: 4, price: 200},
+  ];
+  
+  cartTotal = 0
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.cartItems.forEach(item =>{
+      this.cartTotal += (item.qty * item.price)
+    })
+  }
+
+}
